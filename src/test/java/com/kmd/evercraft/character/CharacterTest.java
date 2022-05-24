fa99b03888188df12e855fd1d15beac5410f34e4
Keith Dingle <keith.dingle@leadingagile.com>
2022-05-24 09:38:10 -0500

    Update CharacterTest.java

--- src/test/java/com/kmd/evercraft/character/CharacterTest.java
+++ src/test/java/com/kmd/evercraft/character/CharacterTest.java
@@ -10,7 +10,7 @@ public class CharacterTest {
     public void armorDefaultsToTen() {
         Character character = new Character();
 
-        assertEquals(10,character.getArmor());
+        assertEquals(9,character.getArmor());
     }
 
     @Test
