04890f254b1334497a574df3d61a73c27afab842
Keith Dingle <keith.dingle@leadingagile.com>
2022-06-14 13:27:06 -0500

    Added MonkBuilder.

--- src/test/java/com/kmd/evercraft/character/MonkTest.java
+++ src/test/java/com/kmd/evercraft/character/MonkTest.java
@@ -1,41 +1,48 @@
 package com.kmd.evercraft.character;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class MonkTest {
+    MonkBuilder monkBuilder = new MonkBuilder();
+
+    @BeforeEach
+    public void resetBuilder() {
+        monkBuilder.reset();
+    }
+
     @Test
     public void monkStartsWithSixHP() {
-        Adventurer monk = new Monk();
+        Adventurer monk = monkBuilder.build();
 
         assertEquals(6,monk.getHitPoints());
     }
 
     @Test
     public void monkIncreaseHPBySixEveryLevel() {
-        Adventurer monk = new Monk();
-        monk.setXP(1000);
-
-        monk.updateCharacter();
+        Adventurer monk = monkBuilder
+                .setXP(1000)
+                .build();
 
         assertEquals(12,monk.getHitPoints());
     }
 
     @Test
     public void positiveWisdomModifierIncreasesArmor() {
-        Adventurer monk = new Monk();
-        monk.setWisdom(12);
-
-        monk.updateCharacter();
+        Adventurer monk = monkBuilder
+                .setWisdom(12)
+                .build();
 
         assertEquals(11,monk.getArmor());
     }
 
     @Test
     public void negativeWisdomModifierKeepsArmorTheSame() {
-        Adventurer monk = new Monk();
-        monk.setWisdom(9);
+        Adventurer monk = monkBuilder
+                .setWisdom(9)
+                .build();
 
         assertEquals(10,monk.getArmor());
     }
